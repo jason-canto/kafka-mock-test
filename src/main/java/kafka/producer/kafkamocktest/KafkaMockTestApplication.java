fc9afa8e31fc1079f47b9a4bb0670091e86c98f5
Jason Canto <jason.canto.ext@sascar.com.br>
2020-07-25 21:32:18 -0300

    first commit

---
+++ src/main/java/kafka/producer/kafkamocktest/KafkaMockTestApplication.java
@@ -0,0 +1,13 @@
+package kafka.producer.kafkamocktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMockTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMockTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kafka/producer/kafkamocktest/KafkaMockTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kafka.producer.kafkamocktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMockTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
